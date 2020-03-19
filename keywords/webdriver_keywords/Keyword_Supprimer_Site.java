fdb27620976be0386de64dd99154b7a2c2cabf11
eiffel-1974 <59851754+eiffel-1974@users.noreply.github.com>
2020-03-19 11:27:53 -0400

    Create Keyword_Supprimer_Site.java

---
+++ keywords/webdriver_keywords/Keyword_Supprimer_Site.java
@@ -0,0 +1,40 @@
+/* Méthode permettant de supprimer un site
+ * vSite nom du site
+ *  vVerification verification du site
+ *  Auteur: Frédéric Leroux
+ *  Date de création: 19 mars 2020
+ *  
+ */
+public static void SupprimerSite(WebDriver driver, String Site) throws Exception {
+	// vSite contient le nom de la site recherché
+	// Cliquer sur le menu Sites pour ouvrir la liste déroulante d'options disponibles 
+	driver.findElement(By.id("HEADER_SITES_MENU_text")).click();
+	// Verrification si le site est dans la liste
+	boolean vVerification = driver.findElements(By.linkText(vSite)).size()!=0;
+	// Si site est dans la liste récent
+		if  (vVerification)
+				  {
+			// Cliquer sur le lien avec le nom du site
+		    driver.findElement(By.xpath("vSite")).click();
+			// Attendre que la page est chargée
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	 	}
+		// Si non, effectuer une recherche du site
+	         else {
+	        	 // Appel de la méthode
+	        	 RechercherSite s = new RechercherSite();
+	        	 RechercherSite.s(driver, vSite);
+	        	 // CLiquer sur le site
+	        	 driver.findElement(By.LinkText("vSite")).click();
+	 			// Attendre que la page est chargée
+	 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	        }
+	// Attendre que la page est chargée
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	// CLiquer sur la rooue dentelé
+    driver.findElement(By.id("HEADER_SITE_CONFIGURATION_DROPDOWN")).click();
+    // CLiquer sur le lien "Supprimer site"
+    driver.findElement(By.id("HEADER_DELETE_SITE_text")).click();
+    // Confirmer la suppression
+    driver.findElement(By.id("ALF_SITE_SERVICE_DIALOG_CONFIRMATION_label")).click();
+  }
